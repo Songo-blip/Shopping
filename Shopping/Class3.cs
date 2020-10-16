c5386fd2b2481b772a8434797c5e843646ce1b8f
Sisipo Songo <ssongo@my365-uwc.ac.za>
2020-10-16 17:58:49 +0200

    Add project files.

---
+++ Shopping/Class3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping
+{
+    public class Item
+    {
+        public string ItemName { get; set; }
+        public decimal ItemPrice { get; set; }
+    }
+}
---
+++ Shopping/Class4.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping
+{
+    public class Tax
+    {
+        public const decimal VatAmount = 15;
+    }
+}
